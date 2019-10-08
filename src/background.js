fd6b876a255bcb604e43dad9246d151c6a217cb5
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-08 16:42:13 +0900

    fix: 메인 페이지가 아니더라도 이미 베네듀 탭 있으면 이동

--- src/background.js
+++ src/background.js
@@ -1,10 +1,13 @@
-const url = 'https://benedu.co.kr/Views/01_Students/00StdHome.aspx'
 chrome.commands.onCommand.addListener(function (command) {
   if (command == "open-benedu-tab")
-    chrome.tabs.query({ url },
+    chrome.tabs.query({
+      url: 'https://benedu.co.kr/*'
+    },
     function (tabs) {
       if (tabs.length)
         chrome.tabs.update(tabs[0].id, { active: true });
-      else chrome.tabs.create({ url });
+      else chrome.tabs.create({
+        url: 'https://benedu.co.kr/Views/01_Students/00StdHome.aspx'
+      });
     });
 });
