dd69bbeb2589159a6cdba0aae3fce9f940338ba6
JUNO <32605822+junhoyeo@users.noreply.github.com>
2019-10-09 08:44:58 +0900

    style: remove semicolon, use eqeqeq

--- src/background.js
+++ src/background.js
@@ -1,13 +1,13 @@
-chrome.commands.onCommand.addListener(function (command) {
-  if (command == "open-benedu-tab")
+chrome.commands.onCommand.addListener(function (cmd) {
+  if (cmd === "open-benedu-tab")
     chrome.tabs.query({
       url: 'https://benedu.co.kr/*'
     },
     function (tabs) {
       if (tabs.length)
-        chrome.tabs.update(tabs[0].id, { active: true });
+        chrome.tabs.update(tabs[0].id, { active: true })
       else chrome.tabs.create({
         url: 'https://benedu.co.kr/Views/01_Students/00StdHome.aspx'
-      });
-    });
-});
+      })
+    })
+})
