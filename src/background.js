1e95379c356a8a2916a1e8711490e891f17c2616
JUNO <hanaro0704@naver.com>
2020-03-18 08:54:49 +0900

    fix: update url as benedu renewal

--- src/background.js
+++ src/background.js
@@ -7,7 +7,7 @@ chrome.commands.onCommand.addListener(function (cmd) {
       if (tabs.length)
         chrome.tabs.update(tabs[0].id, { active: true })
       else chrome.tabs.create({
-        url: 'https://benedu.co.kr/Views/01_Students/00StdHome.aspx'
+        url: 'https://www.benedu.co.kr/StudentHome'
       })
     })
 })
